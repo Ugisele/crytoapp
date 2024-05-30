64776dab7781fb019ec4ea93cb79b71904b64720
Ugisele <Umutonigisele12@gmail.com>
2024-05-30 19:20:23 +0200

    new

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -2,26 +2,25 @@ import 'package:flutter/material.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       home: Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      child: Stack(children: [
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height /1.6,
+      home: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 1.6,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 1.6,
+            )
+          ],
         ),
-        Container(
-           width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height / 1.6,
-        )
-      ],
       ),
     );
-    )
-   
   }
 }
